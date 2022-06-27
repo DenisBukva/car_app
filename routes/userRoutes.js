f9052d2de17202790202efa867f3503880e3c3cf
DenisBukva <denisbukva@hotmail.com>
2022-06-27 10:05:45 +0200

    Smal changes in routes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {loginUser, registerUser, updateUserProfile} from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js'
+import protect  from '../middleware/authMiddleware.js'
 
 const router = express()
 
